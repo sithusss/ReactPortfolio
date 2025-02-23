f320e89fa216e4340269eef8131fb27a438c08f5
Sandali Sithumani <sithumanisandali@gmail.com>
2025-02-23 23:54:45 +0530

    Add image and content
    Generally complete the content by adding images and details for Intro page and footer. But some styling have to do

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "../styles/Footer.css";
+import ReactJS from "../assets/logos/react.png";
+import CSS3 from "../assets/logos/css.png";
+import JWT from "../assets/logos/jwt.png";
+import MongoDB from "../assets/logos/mongo.png";
+import NodeJS from "../assets/logos/node.png";
 
 
 const Footer = () =>{
@@ -10,10 +15,11 @@ const Footer = () =>{
             </div>
             <div className="footer-right">
                 <h3>Developed using..</h3>
-                <img src="" alt="ReactJS" className="tech-img" />
-                <img src="" alt="CSS3" className="tech-img" />
-                <img src="" alt="JWT" className="tech-img" />
-                <img src="" alt="mongoDB" className="tech-img" />
+                <img src={ReactJS} alt="ReactJS" className="tech-img" />
+                <img src={CSS3} alt="CSS3" className="tech-img" />
+                <img src={JWT} alt="JWT" className="tech-img" />
+                <img src={MongoDB} alt="mongoDB" className="tech-img" />
+                <img src={NodeJS} alt="nodeJS" className="tech-img" />
             </div>
         </div>  
 
